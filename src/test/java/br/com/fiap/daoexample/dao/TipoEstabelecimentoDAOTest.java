ffd8852363bc7fbb6561341db8a29ca9d6693054
Sidharta Rezende <sidharta.rezende@c6bank.com>
2021-02-07 15:02:59 -0300

    junit testing

---
+++ src/test/java/br/com/fiap/daoexample/dao/TipoEstabelecimentoDAOTest.java
@@ -0,0 +1,43 @@
+package br.com.fiap.daoexample.dao;
+
+import br.com.fiap.daoexample.cfg.HibernateUtil;
+import br.com.fiap.daoexample.domain.Avaliacao;
+import br.com.fiap.daoexample.domain.TipoEstabelecimento;
+import org.junit.jupiter.api.*;
+
+import javax.persistence.EntityManager;
+
+public class TipoEstabelecimentoDAOTest {
+
+    private static EntityManager em;
+
+    @BeforeAll
+    public static void setup() {
+        em = HibernateUtil.getEntityManager();
+        System.out.println("SessionFactory created");
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        if (em != null) em.close();
+        System.out.println("SessionFactory destroyed");
+    }
+
+
+    @Test()
+    @DisplayName("dado um tipo de estabelecimento, quando tenta gravar , deve cadastrar o tipo de estabelecimento no banco")
+    public void cadastrarBuscar() {
+        TipoEstabelecimentoDAO dao = new TipoEstabelecimentoDAO(em);
+        TipoEstabelecimento tipo = new TipoEstabelecimento();
+        tipo.setNome("Bar e Restaurante");
+        dao.cadastrar(tipo);
+        try {
+            dao.commit();
+            TipoEstabelecimento inserted = dao.buscar(1);
+            Assertions.assertEquals(tipo.getNome(), inserted.getNome());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
